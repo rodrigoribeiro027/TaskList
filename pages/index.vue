83162fb74b883a4cea9e5de94ab68da260cb636d
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2023-11-12 11:48:44 -0300

    update

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <h1>Lista de Tarefas</h1>
+        <p>Criar lista de Tarefas</p>
+
+        <div class="create-new">
+            <input type="text" v-model="newTask" placeholder="Adicionar Nova Tarefa" @keypress.enter="addTask" />
+            <button @click="addTask">Adicionar</button>
+        </div>
+
+        <div class="tasks">
+            <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+        </div>
+
+    </main>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            newTask: ''
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask) {
+                this.$store.commit('ADD_TASK', this.newTask);
+                this.newTask = '';
+            }
+        }
+    }
+}
+</script>
